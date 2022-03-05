283a13ccf7894abd62935e773bb3cbe79c5d7fbb
25prajwal <94011967+25prajwal@users.noreply.github.com>
2022-03-05 12:16:04 +0530

    Update urls.py

--- CollegeERP/urls.py
+++ CollegeERP/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('info/', include('info.urls')),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='info/login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(template_name='info/logout.html'), name='logout'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+]
 ]
